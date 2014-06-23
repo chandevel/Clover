f21c24289560ff5e3e5c69c7935a047c0c0ba29e
Florens Douwes <floens@gmail.com>
2014-06-23 22:19:23 +0200

    Fixed NPE

--- Clover/app/src/main/java/org/floens/chan/ui/service/WatchNotifier.java
+++ Clover/app/src/main/java/org/floens/chan/ui/service/WatchNotifier.java
@@ -207,7 +207,11 @@ public class WatchNotifier extends Service {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         builder.setContentIntent(pendingIntent);
 
-        builder.setTicker(tickerText.subSequence(0, Math.min(tickerText.length(), 50)));
+        if (tickerText != null) {
+            tickerText = tickerText.substring(0, Math.min(tickerText.length(), 50));
+        }
+
+        builder.setTicker(tickerText);
         builder.setContentTitle(title);
         builder.setContentText(content);
         builder.setContentInfo(count);
