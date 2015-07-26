2a9b0aa93ee2e6a7c7cb4bdf79fde130d2a044f1
Nikola Yanakiev <nikola.yanakiev0@gmail.com>
2015-07-26 19:05:36 +0300

    Add selectable text in posts

--- Clover/app/src/main/java/org/floens/chan/ui/cell/PostCell.java
+++ Clover/app/src/main/java/org/floens/chan/ui/cell/PostCell.java
@@ -256,6 +256,12 @@ public class PostCell extends LinearLayout implements PostCellInterface, PostLin
         return post;
     }
 
+    public void setSelectableText(boolean selectable) {
+        if(comment != null)
+            comment.setTextIsSelectable(selectable);
+
+    }
+
     public ThumbnailView getThumbnailView() {
         return thumbnailView;
     }
