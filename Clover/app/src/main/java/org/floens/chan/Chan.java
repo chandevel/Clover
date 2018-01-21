d5db02f30f84835f899c0201154f65834aa2c2f5
Floens <floens@gmail.com>
2018-01-21 22:16:46 +0100

    disable webview debugging by default.

--- Clover/app/src/main/java/org/floens/chan/Chan.java
+++ Clover/app/src/main/java/org/floens/chan/Chan.java
@@ -25,7 +25,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
-import android.webkit.WebView;
 
 import org.codejargon.feather.Feather;
 import org.floens.chan.core.database.DatabaseManager;
@@ -116,7 +115,7 @@ public class Chan extends Application implements UserAgentProvider, Application.
 
             //noinspection PointlessBooleanExpression
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                WebView.setWebContentsDebuggingEnabled(true);
+//                WebView.setWebContentsDebuggingEnabled(true);
             }
         }
     }
