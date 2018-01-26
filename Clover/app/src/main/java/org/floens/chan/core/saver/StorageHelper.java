3cc3c6a0bc73270f5d3a6a800ac1b5a3627cf879
Floens <floens@gmail.com>
2018-01-26 10:38:44 +0100

    Revert "storage: add another directory to the blacklist"
    This reverts commit f043423d6ef89ddf129a9acf10fbc3c9898d126c.

--- Clover/app/src/main/java/org/floens/chan/core/saver/StorageHelper.java
+++ Clover/app/src/main/java/org/floens/chan/core/saver/StorageHelper.java
@@ -31,9 +31,11 @@ public class StorageHelper {
         String absolutePath = file.getAbsolutePath();
         switch (absolutePath) {
             case "/storage":
+                return true;
             case "/storage/emulated":
-            case "/storage/emulated/0":
+                return true;
             case "/storage/emulated/0/0":
+                return true;
             case "/storage/emulated/legacy":
                 return true;
         }
