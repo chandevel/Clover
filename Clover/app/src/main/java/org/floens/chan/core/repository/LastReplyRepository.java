7c11daad6ade1fff6febdf44819a7e3eedffdbd2
Adamantcheese <Adamantcheese@users.noreply.github.com>
2019-04-20 16:38:04 -0700

    Makes it so post timers are per-board

---
+++ Clover/app/src/main/java/org/floens/chan/core/repository/LastReplyRepository.java
@@ -0,0 +1,76 @@
+/*
+ * Clover - 4chan browser https://github.com/Floens/Clover/
+ * Copyright (C) 2014  Floens
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.floens.chan.core.repository;
+
+import org.floens.chan.core.model.orm.Board;
+import org.floens.chan.core.site.Site;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class LastReplyRepository {
+    private static final String TAG = "LastReplyRepository";
+
+    private Map<SiteBoard, Long> lastReplyMap = new HashMap<>();
+
+    @Inject
+    public LastReplyRepository() {
+    }
+
+    public void putLastReply(Site s, Board b) {
+        lastReplyMap.put(new SiteBoard(s, b), System.currentTimeMillis());
+    }
+
+    public long getLastReply(Site s, Board b) {
+        Long lastTime = lastReplyMap.get(new SiteBoard(s, b));
+        return lastTime != null ? lastTime : 0L;
+    }
+
+    public boolean canPost(Site s, Board b) {
+        if(getLastReply(s, b) + 60 * 1000 < System.currentTimeMillis()) {
+            return true;
+        }
+        return false;
+    }
+
+    private class SiteBoard {
+        public String site;
+        public String boardCode;
+
+        public SiteBoard (Site site, Board board) {
+            this.site = site.name();
+            this.boardCode = board.code;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if(!(o instanceof SiteBoard)) return false;
+            SiteBoard sb = (SiteBoard) o;
+            return sb.boardCode.equals(this.boardCode) && sb.site.equals(this.site);
+        }
+
+        @Override
+        public int hashCode() {
+            return (site + boardCode).hashCode();
+        }
+    }
+}
--- Clover/app/src/main/java/org/floens/chan/core/settings/ChanSettings.java
+++ Clover/app/src/main/java/org/floens/chan/core/settings/ChanSettings.java
@@ -161,8 +161,6 @@ public class ChanSettings {
 
     public static final BooleanSetting crashReporting;
 
-    public static final LongSetting lastReplyTime;
-
     static {
         SettingProvider p = new SharedPreferencesSettingProvider(AndroidUtils.getPreferences());
 
@@ -252,8 +250,6 @@ public class ChanSettings {
         updateCheckInterval = new LongSetting(p, "update_check_interval", UpdateManager.DEFAULT_UPDATE_CHECK_INTERVAL_MS);
 
         crashReporting = new BooleanSetting(p, "preference_crash_reporting", true);
-
-        lastReplyTime = new LongSetting(p, "last_reply_time", 0L);
 
         // Old (but possibly still in some users phone)
         // preference_board_view_mode default "list"
