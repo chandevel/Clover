612657afa0a6260b8768caca4c58c3d4e4da447a
Adamantcheese <Adamantcheese@users.noreply.github.com>
2019-06-12 20:08:06 -0700

    Closes #17, seperate thread timers for threads, also thread timer obeys images now Fix issue with video file thumbnails crashing the app when the file wasn't a video

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/presenter/ReplyPresenter.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/presenter/ReplyPresenter.java
@@ -212,20 +212,38 @@ public class ReplyPresenter implements AuthenticationLayoutCallback, ImagePickDe
         draft.spoilerImage = draft.spoilerImage && board.spoilers;
         draft.captchaResponse = null;
 
+        //only 4chan seems to have the post delay, this is a hack for that
         if (draft.loadable.site.name().equals("4chan")) {
-            //only 4chan seems to have the post delay, this is a hack for that
-            if (lastReplyRepository.canPost(draft.loadable.site, draft.loadable.board)) {
-                submitOrAuthenticate();
-            } else {
-                long lastPostTime = lastReplyRepository.getLastReply(draft.loadable.site, draft.loadable.board);
-                long waitTime = draft.loadable.board.cooldownReplies;
-                if (draft.loadable.site.name().equals("4chan") && draft.loadable.site.actions().isLoggedIn()) {
-                    waitTime /= 2;
+            if (loadable.isThreadMode()) {
+                if (lastReplyRepository.canPostReply(draft.loadable.site, draft.loadable.board, draft.file != null)) {
+                    submitOrAuthenticate();
+                } else {
+                    long lastPostTime = lastReplyRepository.getLastReply(draft.loadable.site, draft.loadable.board);
+                    long waitTime = draft.file != null ? draft.loadable.board.cooldownImages : draft.loadable.board.cooldownReplies;
+                    if (draft.loadable.site.actions().isLoggedIn()) {
+                        waitTime /= 2;
+                    }
+                    long timeLeft = waitTime - ((System.currentTimeMillis() - lastPostTime) / 1000L);
+                    String errorMessage = getAppContext().getString(R.string.reply_error_message_timer_reply, timeLeft);
+                    switchPage(Page.INPUT, true);
+                    callback.openMessage(true, false, errorMessage, true);
                 }
-                long timeLeft = waitTime - ((System.currentTimeMillis() - lastPostTime) / 1000L);
-                String errorMessage = getAppContext().getString(R.string.reply_error_message_timer, timeLeft);
-                switchPage(Page.INPUT, true);
-                callback.openMessage(true, false, errorMessage, true);
+            } else if (loadable.isCatalogMode()) {
+                if (lastReplyRepository.canPostThread(draft.loadable.site, draft.loadable.board)) {
+                    submitOrAuthenticate();
+                } else {
+                    long lastThreadTime = lastReplyRepository.getLastThread(draft.loadable.site, draft.loadable.board);
+                    long waitTime = draft.loadable.board.cooldownThreads;
+                    if (draft.loadable.site.actions().isLoggedIn()) {
+                        waitTime /= 2;
+                    }
+                    long timeLeft = waitTime - ((System.currentTimeMillis() - lastThreadTime) / 1000L);
+                    String errorMessage = getAppContext().getString(R.string.reply_error_message_timer_thread, timeLeft);
+                    switchPage(Page.INPUT, true);
+                    callback.openMessage(true, false, errorMessage, true);
+                }
+            } else {
+                Logger.wtf(TAG, "Loadable isn't a thread or a catalog loadable????");
             }
         } else {
             submitOrAuthenticate();
@@ -243,7 +261,11 @@ public class ReplyPresenter implements AuthenticationLayoutCallback, ImagePickDe
     @Override
     public void onPostComplete(HttpCall httpCall, ReplyResponse replyResponse) {
         if (replyResponse.posted) {
-            lastReplyRepository.putLastReply(draft.loadable.site, draft.loadable.board);
+            if (loadable.isThreadMode()) {
+                lastReplyRepository.putLastReply(draft.loadable.site, draft.loadable.board);
+            } else if (loadable.isCatalogMode()) {
+                lastReplyRepository.putLastThread(draft.loadable.site, draft.loadable.board);
+            }
 
             if (ChanSettings.postPinThread.get()) {
                 if (loadable.isThreadMode()) {
