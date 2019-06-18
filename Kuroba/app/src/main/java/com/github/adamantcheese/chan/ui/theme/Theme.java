e81f3e1374b6896da211f0320c6d432d7ccdcf72
Adamantcheese <Adamantcheese@users.noreply.github.com>
2019-06-17 19:37:56 -0700

    Cleanup due to UI changes

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/ui/theme/Theme.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/ui/theme/Theme.java
@@ -68,7 +68,6 @@ public class Theme {
     public ThemeDrawable backDrawable;
     public ThemeDrawable doneDrawable;
     public ThemeDrawable historyDrawable;
-    public ThemeDrawable listAddDrawable;
     public ThemeDrawable helpDrawable;
     public ThemeDrawable refreshDrawable;
 
@@ -91,7 +90,6 @@ public class Theme {
         backDrawable = new ThemeDrawable(R.drawable.ic_arrow_back_black_24dp, 0.54f);
         doneDrawable = new ThemeDrawable(R.drawable.ic_done_black_24dp, 0.54f);
         historyDrawable = new ThemeDrawable(R.drawable.ic_history_black_24dp, 0.54f);
-        listAddDrawable = new ThemeDrawable(R.drawable.ic_playlist_add_black_24dp, 0.54f);
         helpDrawable = new ThemeDrawable(R.drawable.ic_help_outline_black_24dp, 0.54f);
         refreshDrawable = new ThemeDrawable(R.drawable.ic_refresh_black_24dp, 0.54f);
     }
