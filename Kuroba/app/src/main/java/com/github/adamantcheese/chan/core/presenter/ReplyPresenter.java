6ca7d5d3e7fd222dc29194b99bb081248ad63423
Adamantcheese <Adamantcheese@users.noreply.github.com>
2019-07-02 16:34:31 -0700

    Fixes #163

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/presenter/ReplyPresenter.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/presenter/ReplyPresenter.java
@@ -263,9 +263,9 @@ public class ReplyPresenter implements AuthenticationLayoutCallback, ImagePickDe
     public void onPostComplete(HttpCall httpCall, ReplyResponse replyResponse) {
         if (replyResponse.posted) {
             if (loadable.isThreadMode()) {
-                lastReplyRepository.putLastReply(loadable.site, draft.loadable.board);
+                lastReplyRepository.putLastReply(loadable.site, loadable.board);
             } else if (loadable.isCatalogMode()) {
-                lastReplyRepository.putLastThread(loadable.site, draft.loadable.board);
+                lastReplyRepository.putLastThread(loadable.site, loadable.board);
             }
 
             if (ChanSettings.postPinThread.get()) {
