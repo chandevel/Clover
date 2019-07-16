abbf9ade1fad68373ab91638ac1e8c598327bda5
k1rakishou <kez1911@protonmail.com>
2019-07-16 21:39:54 +0300

    Trigger CI build

--- Kuroba/scripts/update-apk.sh
+++ Kuroba/scripts/update-apk.sh
@@ -2,11 +2,11 @@
 git config --global user.name "Travis CI"
 git config --global user.email "noreply+travis@fossasia.org"
 
-git clone --quiet --branch=apk https://fossasia:$GITHUB_API_KEY@github.com/fossasia/open-event-android apk > /dev/null
+git clone --quiet --branch=apk https://github.com/K1rakishou/Kuroba.git apk > /dev/null
 cd apk
-\cp -r ../app/build/outputs/apk/*/**.apk .
-\cp -r ../app/build/outputs/apk/debug/output.json debug-output.json
-\cp -r ../app/build/outputs/apk/release/output.json release-output.json
+\cp -r ../*/**.apk .
+\cp -r ../debug/output.json debug-output.json
+\cp -r ../release/output.json release-output.json
 
 git checkout --orphan temporary
 
@@ -16,4 +16,4 @@ git commit -am "[Auto] Update Test Apk ($(date +%Y-%m-%d.%H:%M:%S))"
 git branch -D apk
 git branch -m apk
 
-git push origin apk --force --quiet > /dev/null 
+git push origin apk --force --quiet > /dev/null
