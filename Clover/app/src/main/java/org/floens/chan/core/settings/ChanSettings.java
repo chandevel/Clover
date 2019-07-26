afee7c7ba2f627fcbaf147e2cdde8c5ed41ef7fd
Floens <floens@gmail.com>
2019-07-26 18:07:03 +0200

    captcha: remove leftover strings and settings for defunct google cookie functionality

--- Clover/app/src/main/java/org/floens/chan/core/settings/ChanSettings.java
+++ Clover/app/src/main/java/org/floens/chan/core/settings/ChanSettings.java
@@ -161,8 +161,6 @@ public class ChanSettings {
 
     public static final BooleanSetting crashReporting;
     public static final BooleanSetting useNewCaptchaWindow;
-    public static final StringSetting googleCookie;
-    public static final LongSetting lastGoogleCookieUpdateTime;
 
     static {
         SettingProvider p = new SharedPreferencesSettingProvider(AndroidUtils.getPreferences());
@@ -254,8 +252,6 @@ public class ChanSettings {
 
         crashReporting = new BooleanSetting(p, "preference_crash_reporting", true);
         useNewCaptchaWindow = new BooleanSetting(p, "use_new_captcha_window", true);
-        googleCookie = new StringSetting(p, "google_cookie", "");
-        lastGoogleCookieUpdateTime = new LongSetting(p, "last_google_cookie_update_time", 0L);
 
         // Old (but possibly still in some users phone)
         // preference_board_view_mode default "list"
