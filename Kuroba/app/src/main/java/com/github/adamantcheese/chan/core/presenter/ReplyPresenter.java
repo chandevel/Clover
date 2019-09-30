266d9ad271b0f694c81c7b63480329f441aeeff5
Adamantcheese <Adamantcheese@users.noreply.github.com>
2019-09-29 23:32:34 -0700

    Closes #352, I have no fucking clue why but ok

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/presenter/ReplyPresenter.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/presenter/ReplyPresenter.java
@@ -560,7 +560,9 @@ public class ReplyPresenter implements AuthenticationLayoutCallback, ImagePickDe
                     // cleanup resources tied to the new captcha layout/presenter
                     callback.destroyCurrentAuthentication();
                     callback.initializeAuthentication(loadable.site, authentication, this, useV2NoJsCaptcha, autoReply);
-                    callback.setPage(Page.AUTHENTICATION);
+                    if(!autoReply) {
+                        callback.setPage(Page.AUTHENTICATION);
+                    }
                     break;
             }
         }
