be4fad7bfa57b1f95e88edf9bd95d0a10690beb9
k1rakishou <kez1911@protonmail.com>
2019-12-14 19:13:33 +0300

    (#431) Move webm streaming setting to the experimental settings

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/ui/controller/settings/ExperimentalSettingsController.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/ui/controller/settings/ExperimentalSettingsController.java
@@ -136,6 +136,10 @@ public class ExperimentalSettingsController
                 R.string.add_dubs_description
         )));
 
+        group.add(new BooleanSettingView(this, ChanSettings.videoStream,
+                R.string.setting_video_stream,
+                R.string.setting_video_stream_description));
+
         groups.add(group);
     }
 }
