a13aaf464b4bf801a5d193703d43d65074399248
Adamantcheese <Adamantcheese@users.noreply.github.com>
2020-02-02 21:16:29 -0800

    Change strings, unfuck something I forget months ago when I did youtube api key settings

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/ui/controller/settings/BehaviourSettingsController.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/ui/controller/settings/BehaviourSettingsController.java
@@ -212,6 +212,8 @@ public class BehaviourSettingsController
                     "Youtube API Key",
                     "Youtube API Key"
             ));
+
+            groups.add(other);
         }
     }
 
