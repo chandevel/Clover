08bc780a106c105ce8c84a3b9f22f4b1b34c9ca8
k1rakishou <kez1911@protonmail.com>
2020-02-08 20:48:18 +0300

    Use debouncer with 1 second debounce delay before updating pins state.
    Because it was called way too often (it could get called like 10 times per second if you had lots of active pins) which caused start/stop service getting called too often which caused notification
    getting updated too often etc. Sometimes, this would cause the notification to become a silent notification. Now we wait for at least 1 second without any pin updates before doing pins update.

---
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/base/Debouncer.kt
@@ -0,0 +1,27 @@
+package com.github.adamantcheese.chan.core.base
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.atomic.AtomicBoolean
+
+open class Debouncer(
+        private val eagerInitialization: Boolean
+) {
+    private val eagerlyInitialized = AtomicBoolean(!eagerInitialization)
+    private val handler: Handler = Handler(Looper.getMainLooper())
+
+    open fun post(runnable: Runnable, delayMs: Long) {
+        if (eagerInitialization && !eagerlyInitialized.get()) {
+            handler.post {
+                if (eagerlyInitialized.compareAndSet(false, true)) {
+                    runnable.run()
+                }
+            }
+
+            return
+        }
+
+        handler.removeCallbacksAndMessages(null)
+        handler.postDelayed(runnable, delayMs)
+    }
+}
