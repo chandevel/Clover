b41488f89ca9a70a314a8b3e3fe9e7a43d04eedc
Adamantcheese <Adamantcheese@users.noreply.github.com>
2020-05-06 20:45:39 -0700

    Closes #873

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/utils/PostUtils.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/utils/PostUtils.java
@@ -16,16 +16,20 @@ import java.util.Set;
 public class PostUtils {
 
     //https://programming.guide/java/formatting-byte-size-to-human-readable-format.html
+    // ALL sites use KiB, displayed more formally as KB, not the SI unit kB
     public static String getReadableFileSize(long bytes) {
-        if (-1000 < bytes && bytes < 1000) {
+        long absB = bytes == Long.MIN_VALUE ? Long.MAX_VALUE : Math.abs(bytes);
+        if (absB < 1024) {
             return bytes + " B";
         }
-        CharacterIterator ci = new StringCharacterIterator("kMGTPE");
-        while (bytes <= -999_950 || bytes >= 999_950) {
-            bytes /= 1000;
+        long value = absB;
+        CharacterIterator ci = new StringCharacterIterator("KMGTPE");
+        for (int i = 40; i >= 0 && absB > 0xfffccccccccccccL >> i; i -= 10) {
+            value >>= 10;
             ci.next();
         }
-        return String.format(Locale.ENGLISH, "%.1f %cB", bytes / 1000.0, ci.current());
+        value *= Long.signum(bytes);
+        return String.format(Locale.ENGLISH, "%.1f %ciB", value / 1024.0, ci.current());
     }
 
     public static Post findPostById(int id, @Nullable ChanThread thread) {
