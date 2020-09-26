ba09350555d1c4ab47ce66624c347709f3b3753f
Adamantcheese <Adamantcheese@users.noreply.github.com>
2020-09-25 20:50:01 -0700

    Add in some stuff for parsing, should have all the stuff needed now? Still not working though

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/site/FoolFuukaArchive.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/site/FoolFuukaArchive.java
@@ -1,9 +1,11 @@
 package com.github.adamantcheese.chan.core.site;
 
 import android.util.JsonReader;
+import android.util.JsonToken;
 
 import com.github.adamantcheese.chan.core.manager.ArchivesManager;
 import com.github.adamantcheese.chan.core.model.Post;
+import com.github.adamantcheese.chan.core.model.PostImage;
 import com.github.adamantcheese.chan.core.model.orm.Board;
 import com.github.adamantcheese.chan.core.model.orm.Loadable;
 import com.github.adamantcheese.chan.core.site.common.DefaultPostParser;
@@ -14,8 +16,10 @@ import com.github.adamantcheese.chan.core.site.parser.ChanReaderProcessingQueue;
 import com.github.adamantcheese.chan.core.site.parser.CommentParser;
 import com.github.adamantcheese.chan.core.site.parser.PostParser;
 import com.github.adamantcheese.chan.utils.NetUtils;
+import com.github.adamantcheese.chan.utils.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import okhttp3.HttpUrl;
@@ -48,11 +52,10 @@ public class FoolFuukaArchive {
 
                     }
                 },
-                (NetUtils.JsonParser<ChanLoaderResponse>) reader -> new ChanReaderParser(Loadable.forThread(board,
-                        opNo,
-                        "",
-                        false
-                ), new ArrayList<>(), new FoolFuukaReader()).parse(reader)
+                reader -> new ChanReaderParser(Loadable.forThread(board, opNo, "", false),
+                        new ArrayList<>(),
+                        new FoolFuukaReader()
+                ).parse(reader)
         );
     }
 
@@ -71,19 +74,21 @@ public class FoolFuukaArchive {
                 JsonReader reader, ChanReaderProcessingQueue queue
         )
                 throws Exception {
-            reader.beginObject();
-            reader.nextString();
+            reader.beginObject(); // start JSON
+
+            reader.nextString(); // the op number, duplicated in the post object itself
             reader.beginObject();
             // OP object
             readPostObject(reader, queue);
             reader.endObject();
+
             reader.beginObject();
             // Posts object
             while (reader.hasNext()) {
                 readPostObject(reader, queue);
             }
-
             reader.endObject();
+
             reader.endObject();
         }
 
@@ -98,16 +103,20 @@ public class FoolFuukaArchive {
         )
                 throws Exception {
             Post.Builder builder = new Post.Builder();
-            reader.nextName(); // op or post number; not necessary as it's in the rest of the data
-            reader.beginObject();
+            builder.board(queue.getLoadable().board);
+            reader.nextName(); // "op" or post number; not necessary as it's in the rest of the data so ignore this
+            reader.beginObject(); // post object itself
             while (reader.hasNext()) {
                 String key = reader.nextName();
                 switch (key) {
                     case "num":
                         builder.id(reader.nextInt());
                         break;
+                    case "thread_num":
+                        builder.opId(reader.nextInt());
                     case "op":
                         builder.op(reader.nextInt() == 1);
+                        queue.setOp(builder);
                         break;
                     case "timestamp":
                         builder.setUnixTimestampSeconds(reader.nextLong());
@@ -119,59 +128,71 @@ public class FoolFuukaArchive {
                         builder.name(reader.nextString());
                         break;
                     case "trip":
-                        builder.tripcode(reader.nextString());
+                        if (reader.peek() == JsonToken.NULL) {
+                            reader.nextNull();
+                        } else {
+                            builder.tripcode(reader.nextString());
+                        }
                         break;
-                    case "title_processed":
-                        if (builder.op) {
-                            queue.getLoadable().title = reader.nextString();
+                    case "title":
+                        if (builder.op && reader.peek() != JsonToken.NULL) {
+                            String title = reader.nextString();
+                            builder.subject(title);
+                            queue.getLoadable().title = title;
                         } else {
                             reader.skipValue();
                         }
                         break;
                     case "comment_processed":
                         String comment = reader.nextString();
-
+                        comment = comment.replace("\\n", "");
+                        builder.comment(comment);
                         break;
                     case "sticky":
-                        boolean sticky = reader.nextInt() == 1;
+                        builder.sticky(reader.nextInt() == 1);
                         break;
                     case "locked":
-                        boolean locked = reader.nextInt() == 1;
-                        break;
-                    case "deleted":
-                        boolean deleted = reader.nextInt() == 1;
+                        builder.closed(reader.nextInt() == 1);
                         break;
                     case "media":
+                        PostImage.Builder imageBuilder = new PostImage.Builder();
                         reader.beginObject();
                         while (reader.hasNext()) {
                             String mediaKey = reader.nextName();
                             switch (mediaKey) {
                                 case "spoiler":
-                                    boolean spoiler = reader.nextInt() == 1;
+                                    imageBuilder.spoiler(reader.nextInt() == 1);
                                     break;
                                 case "media_w":
-                                    int width = reader.nextInt();
+                                    imageBuilder.imageWidth(reader.nextInt());
                                     break;
                                 case "media_h":
-                                    int height = reader.nextInt();
+                                    imageBuilder.imageHeight(reader.nextInt());
                                     break;
                                 case "media_size":
-                                    long size = reader.nextLong();
+                                    imageBuilder.size(reader.nextLong());
                                     break;
+                                case "media_filename":
+                                    String filename = reader.nextString();
+                                    imageBuilder.filename(StringUtils.removeExtensionFromFileName(filename));
+                                    imageBuilder.extension(StringUtils.extractFileNameExtension(filename));
                                 case "media_hash":
-                                    String hash = reader.nextString();
+                                    imageBuilder.fileHash(reader.nextString(), true);
                                     break;
                                 case "media_link":
-                                    HttpUrl fullsize = HttpUrl.get(reader.nextString());
+                                    imageBuilder.imageUrl(HttpUrl.get(reader.nextString()));
                                     break;
+                                case "media_orig":
+                                    imageBuilder.serverFilename(StringUtils.removeExtensionFromFileName(reader.nextString()));
                                 case "thumb_link":
-                                    HttpUrl thumbnail = HttpUrl.get(reader.nextString());
+                                    imageBuilder.thumbnailUrl(HttpUrl.get(reader.nextString()));
                                     break;
                                 default:
                                     reader.skipValue();
                             }
                         }
                         reader.endObject();
+                        builder.images(Collections.singletonList(imageBuilder.build()));
                         break;
                     default:
                         reader.skipValue();
@@ -179,14 +200,6 @@ public class FoolFuukaArchive {
                 }
             }
             reader.endObject();
-
-            if (builder.op) {
-                // Update OP fields later on the main thread
-                builder.closed(builder.closed);
-                builder.archived(builder.archived);
-                builder.sticky(builder.sticky);
-                queue.setOp(builder);
-            }
 
             queue.addForParse(builder);
         }
