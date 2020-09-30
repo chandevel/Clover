8f35b0a07ce84073699296d434c8d5516b8ff91f
Adamantcheese <Adamantcheese@users.noreply.github.com>
2020-09-29 19:23:25 -0700

    Cleanup to make this nicer while trying to figure out some Soundcloud stuff; probably not easy right now afaict

--- Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/repository/BitmapRepository.java
+++ Kuroba/app/src/main/java/com/github/adamantcheese/chan/core/repository/BitmapRepository.java
@@ -1,15 +1,22 @@
 package com.github.adamantcheese.chan.core.repository;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+
+import androidx.annotation.DrawableRes;
 
 import com.github.adamantcheese.chan.R;
 
 public class BitmapRepository {
 
+    @SuppressLint("StaticFieldLeak")
+    private static Context context;
+
     public static Bitmap youtubeIcon;
     public static Bitmap streamableIcon;
+
     public static Bitmap stickyIcon;
     public static Bitmap closedIcon;
     public static Bitmap trashIcon;
@@ -17,14 +24,23 @@ public class BitmapRepository {
     public static Bitmap partyHat;
     public static Bitmap error;
 
-    public static void initialize(Context context) {
-        youtubeIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.youtube_icon);
-        streamableIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.streamable_icon);
-        stickyIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.sticky_icon);
-        closedIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.closed_icon);
-        trashIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.trash_icon);
-        archivedIcon = BitmapFactory.decodeResource(context.getResources(), R.drawable.archived_icon);
-        partyHat = BitmapFactory.decodeResource(context.getResources(), R.drawable.partyhat);
-        error = BitmapFactory.decodeResource(context.getResources(), R.drawable.error_icon);
+    public static void initialize(Context c) {
+        context = c;
+
+        youtubeIcon = decode(R.drawable.youtube_icon);
+        streamableIcon = decode(R.drawable.streamable_icon);
+
+        stickyIcon = decode(R.drawable.sticky_icon);
+        closedIcon = decode(R.drawable.closed_icon);
+        trashIcon = decode(R.drawable.trash_icon);
+        archivedIcon = decode(R.drawable.archived_icon);
+        partyHat = decode(R.drawable.partyhat);
+        error = decode(R.drawable.error_icon);
+
+        context = null; // to prevent leaks
+    }
+
+    private static Bitmap decode(@DrawableRes int resId) {
+        return BitmapFactory.decodeResource(context.getResources(), resId);
     }
 }
